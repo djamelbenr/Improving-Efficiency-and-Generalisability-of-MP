a21f88e67c6eebec222dbd683e8e471d874cf951
djamelben <djamelben.univ@gmail.com>
2024-01-11 16:57:40 +1000

    Update attention.py

--- modules/attention.py
+++ modules/attention.py
@@ -1,1 +1,22 @@
+#!/usr/bin/env python3.8
+# -*- coding: utf-8 -*-
+
+## Attention function 
+
+"""
+Created on Jan Thu 11 2024
+@author: Djamel Eddine Benrachou, et al
+"""
+
+import torch 
+from torch import nn 
+from torchgviz import make_dot
+from torch.distributions import Normal, OneHotCategorical
+import torch.nn.functional as F
+import numpy as np 
+import math 
+import matplotlib.pyplot as plt 
+
+
+
 
