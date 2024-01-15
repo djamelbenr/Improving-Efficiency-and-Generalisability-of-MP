1ebfa9f3f9f96970a51c0dde95501ea0e106226f
djamelben <djamelben.univ@gmail.com>
2024-01-15 10:43:26 +1000

    Update attention.py

--- modules/attention.py
+++ modules/attention.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 """
 Attention required modules.
 1.dot_scaled_product
-2.
+2.expand_mask (optional)
 3.
 4.multi-head attention encoder
 """
@@ -37,8 +37,62 @@ def scaled_dot_product(q, k, v, mask=None):
 
 #-- masked attention can be added here.
 def expand_mask(mask):
-  assert mask.ndim >=2, 
-  ##Mask must be at least 2-dimensional with seq_length x seq_length
+    assert mask.ndim >=2, 
+      ##Mask must be at least 2-dimensional with seq_length x seq_length
+    if mask.ndim==3:
+        mask = mask.unsqueeze(1)
+    while mask.ndim < 4:
+        mask = mask.unsqueeze(0)
+    return mask
+
+class MultiheadAttention(nn.Module):
+
+    def __init__(self, input_dim, embed_dim, num_heads):
+        super().__init__()
+        assert embed_dim % num_heads == 0, "Embedding dimension must be 0 modulo number of heads."
+
+        self.embed_dim = embed_dim
+        self.num_heads = num_heads
+        self.head_dim = embed_dim // num_heads
+
+        # Stack all weight matrices 1...h together for efficiency
+        # Note that in many implementations you see "bias=False" which is optional
+        self.qkv_proj = nn.Linear(input_dim, 3*embed_dim)
+        self.o_proj = nn.Linear(embed_dim, embed_dim)
+
+        self._reset_parameters()
+
+    def _reset_parameters(self):
+        # Original Transformer initialization, see PyTorch documentation
+        nn.init.xavier_uniform_(self.qkv_proj.weight)
+        self.qkv_proj.bias.data.fill_(0)
+        nn.init.xavier_uniform_(self.o_proj.weight)
+        self.o_proj.bias.data.fill_(0)
+
+    def forward(self, x, mask=None, return_attention=False):
+        batch_size, seq_length, _ = x.size()
+        if mask is not None:
+            mask = expand_mask(mask)
+        qkv = self.qkv_proj(x)
+
+        # Separate Q, K, V from linear output
+        qkv = qkv.reshape(batch_size, seq_length, self.num_heads, 3*self.head_dim)
+        qkv = qkv.permute(0, 2, 1, 3) # [Batch, Head, SeqLen, Dims]
+        q, k, v = qkv.chunk(3, dim=-1)
+
+        # Determine value outputs
+        values, attention = scaled_dot_product(q, k, v, mask=mask)
+        values = values.permute(0, 2, 1, 3) # [Batch, SeqLen, Head, Dims]
+        values = values.reshape(batch_size, seq_length, self.embed_dim)
+        o = self.o_proj(values)
+
+        if return_attention:
+            return o, attention
+        else:
+            return o
+
+
+
   
 
 
