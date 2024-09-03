f95a76793c1417d732e3600773c9b7d786b01ef7
djamelben <djamelben.univ@gmail.com>
2024-09-03 18:22:44 +1000

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -65,13 +65,13 @@ def maskedNLL(y_pred, y_gt, mask):
 
 # Negative Log-Likelihood loss function for testing with maneuvers and masking
 def maskedNLLTest(fut_pred, lat_pred, lon_pred, fut, op_mask,
-                    num_lat_classes=3, num_lon_classes=2,
-                    use_maneuvers=True, avg_along_time=False, separately=False):
+                  num_lat_classes=3, num_lon_classes=2,
+                  use_maneuvers=True, avg_along_time=False, separately=False):
     if use_maneuvers:
         acc = torch.zeros(op_mask.shape[0], op_mask.shape[1], num_lon_classes * num_lat_classes, device=fut_pred.device)
         for k in range(num_lon_classes):
             for l in range(num_lat_classes):
-                wts = (lat_pred[:, l] * lon_pred[:, k]).unsqueeze(0).expand_as(fut_pred[0])
+                wts = (lat_pred[:, l] * lon_pred[:, k]).unsqueeze(1).expand_as(fut_pred[k * num_lat_classes + l][:, :, 0:2])
                 y_pred = fut_pred[k * num_lat_classes + l]
                 muX, muY = y_pred[:, :, 0], y_pred[:, :, 1]
                 sigX, sigY = y_pred[:, :, 2], y_pred[:, :, 3]
@@ -128,7 +128,7 @@ def maskedMSE(y_pred, y_gt, mask):
     return torch.sum(mse) / torch.sum(mask)
 
 # MSE loss function for testing with optional separation
-def maskedMSETest(y_pred, y_gt, mask, separately=False):
+def masked_mse_test(y_pred, y_gt, mask, separately=False):
     muX, muY = y_pred[:, :, 0], y_pred[:, :, 1]
     x, y = y_gt[:, :, 0], y_gt[:, :, 1]
 
@@ -149,4 +149,3 @@ def logsumexp(inputs, dim=None, keepdim=False):
     outputs = s + (inputs - s).exp().sum(dim=dim, keepdim=True).log()
     
     return outputs.squeeze(dim) if not keepdim else outputs
-
